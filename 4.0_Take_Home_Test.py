63fba1c2bd48ffd45e5e58cfdc2974944a407668
Marc Hermon <hermonm@urbandaleschools.com>
2019-02-19 22:53:25 -0600

    Update 4.0_Take_Home_Test.py

--- 4.0_Take_Home_Test.py
+++ 4.0_Take_Home_Test.py
@@ -1,143 +1,42 @@
 '''
-HONOR CODE: I solemnly promise that while taking this test I will only use PyCharm or the Internet,
+HONOR CODE: I solemnly promise that while taking this test I will only use PyCharm or the Internet, 
 but I will definitely not ask another person except the instructor. Signed: ______________________
+'''
 
-
-  1. What is missing from this code?
+  #1. Make the following program work.
      
      midichlorians = float(input("Enter midichlorian count: ")
-     if midichlorians > 10000:
+     if midichlorians > 10000
          print("You have serious Jedi potential")
-     else:
+     elif:
          print("Jedi, you will never be.")
-'''
-     
-     
-     
-'''
-  2. This runs, but there is something wrong. What is it?
-     
-     user_input = input("R2D2 is a: ")
-     print("A. Droid")
-     print("B. Storm Trooper")
-     if user_input.upper() == "A":
-         print("Correct!")
-     else:
-         print("Incorrect.")
-'''
 
 
-
-'''
-     
-  3. There are two things wrong with this code that tests if x is set to a
-     positive value. One prevents it from running, and the other is subtle.
-     Make sure the if statement works no matter what x is set to.
-     Identify both issues. 
-     
-     x == 4
-     if x >= 0:
-         print("x is positive.")
-     else:
-         print("x is not positive.")
- '''
- 
- 
- 
- '''
-  4. What three things are wrong with the following code?
+ # 2. Make the following program work.
      
      x = input("Enter a number: ")
      if x = 3
          print("You entered 3")
- '''
- 
- 
- '''
-  5. There are four things wrong with this code. Identify all four issues. 
+
+
+  # 3. Make the following program work. 
      
      answer = input("What is the name of Poe Dameron's Droid? ")
      if a = "BB8":
          print("Correct!")
          else
          print("Incorrect! It is BB8.")
-'''
 
 
-
-'''
-  6. This program doesn't work correctly. What is wrong?
+  # 4. Make the following program work.
      
      x = input("Who are the top 3 greatest Jedi?")
-     if x == "Yoda" or "Luke Skywalker" or "Obi-Wan Kenobi":
-         print("That is correct!")
-'''
+     if jedi == Yoda or Luke Skywalker or Obi-Wan Kenobi:
+         print "That is correct!"
 
 
 
-'''
-  7. Look at the code below. Write your best guess here on what it will print.
-     Next, run the code and see if you are correct.
-     Clearly label your guess and the actual answer.
-     
-     x = 5
-     y = x == 6
-     z = x == 5
-     print("x=", x)
-     print("y=", y)
-     print("z=", z)
-     if y:
-         print("Star Wars Episodes 1,2,3 are the best!")
-     if z:
-         print("Star Wars Episodes 4,5,6 are the best!")
-'''
-
-
-
-
-'''
- 8. Look at the code below. Write you best guess on what it will print.
-     Next, run the code and see if you are correct. 
-     
-     x = 5
-     y = 10
-     z = 10
-     print(x < y)
-     print(y < z)
-     print(x == 5)
-     print(not x == 5)
-     print(x != 5)
-     print(not x != 5)
-     print(x == "5")
-     print(5 == x + 0.00000000001)
-     print(x == 5 and y == 10)
-     print(x == 5 and y == 5)
-     print(x == 5 or y == 5)
-'''
-
-
-
-'''
- 9. Look at the code below. Write you best guess on what it will print.
-     Next, run the code and see if you are correct. (HINT: when comparing strings, ASCII codes are used. https://www.ascii-code.com/)
-     
-     print("3" == "3")
-     print(" 3" == "3")
-     print(3 < 4)
-     print("3" < "4")
-     print("3" < 4)
-     print("<" < ">")
-     print((2 == 2) == "True")
-     print((2 == 2) == True)
-     print("?"<"!")
-'''
-
-
-
-'''
- 10. What things are wrong with this section of code?
-     The programmer wants to set the force sensitivity variable 
-     according to the character the user selects.
+ # 5. Make the following program work whether they enter a, A, Jedi Master or jedi master
      
      print("Welcome to the Jedi Academy!")
 
@@ -153,5 +52,5 @@ but I will definitely not ask another person except the instructor. Signed: ____
          sensitivity = 900
      else if user_input = C:
          sensitivity = 0
-'''
+
 
