3e75ff4894c98cd2d3bd005b8e5d0c7d530b9c52
Superpresident <dariusbrowne12@gmail.com>
2019-02-22 12:16:56 -0600

    ye

--- 4.0_Take_Home_Test.py
+++ 4.0_Take_Home_Test.py
@@ -3,141 +3,42 @@ HONOR CODE: I solemnly promise that while taking this test I will only use PyCha
 but I will definitely not ask another person except the instructor. Signed: ______________________
 
 
-  1. What is missing from this code?
-     
-     midichlorians = float(input("Enter midichlorian count: ")
-     if midichlorians > 10000:
-         print("You have serious Jedi potential")
-     else:
-         print("Jedi, you will never be.")
-'''
-     
-     
-     
-'''
-  2. This runs, but there is something wrong. What is it?
-     
-     user_input = input("R2D2 is a: ")
-     print("A. Droid")
-     print("B. Storm Trooper")
-     if user_input.upper() == "A":
+  #1. Make the following program work.
+
+     midichlorians=float(input("Enter midichlorian count:"))
+if midichlorians > 10000:
+    print("You have serious Jedi potential")
+else:
+    print("Jedi, you will never be.")
+
+ # 2. Make the following program work.
+
+x = int(input("Enter a number: "))
+if x == 3:
+    print("You entered 3")
+
+  # 3. Make the following program work.
+
+   answer = str(input("What is the name of Poe Dameron's Droid? "))
+if answer.upper() == "BB8":
          print("Correct!")
-     else:
-         print("Incorrect.")
-'''
+else:
+    print("Incorrect! It is BB8.")
 
 
 
-'''
-     
-  3. There are two things wrong with this code that tests if x is set to a
-     positive value. One prevents it from running, and the other is subtle.
-     Make sure the if statement works no matter what x is set to.
-     Identify both issues. 
-     
-     x == 4
-     if x >= 0:
-         print("x is positive.")
-     else:
-         print("x is not positive.")
- '''
- 
- 
- 
- '''
-  4. What three things are wrong with the following code?
-     
-     x = input("Enter a number: ")
-     if x = 3
-         print("You entered 3")
- '''
- 
- 
- '''
-  5. There are four things wrong with this code. Identify all four issues. 
-     
-     answer = input("What is the name of Poe Dameron's Droid? ")
-     if a = "BB8":
-         print("Correct!")
-         else
-         print("Incorrect! It is BB8.")
-'''
+  # 4. Make the following program work.
+
+   jedi = input("Who are the top 3 greatest Jedi?\n")
+if jedi.lower() == "yoda" \
+        or jedi.lower() == "luke skywalker" \
+        or jedi.lower() =="obi-wan kenobi":
+    print("That is correct!")
 
 
 
-'''
-  6. This program doesn't work correctly. What is wrong?
-     
-     x = input("Who are the top 3 greatest Jedi?")
-     if x == "Yoda" or "Luke Skywalker" or "Obi-Wan Kenobi":
-         print("That is correct!")
-'''
+ # 5. Make the following program work whether they enter a, A, Jedi Master or jedi master
 
-
-
-'''
-  7. Look at the code below. Write your best guess here on what it will print.
-     Next, run the code and see if you are correct.
-     Clearly label your guess and the actual answer.
-     
-     x = 5
-     y = x == 6
-     z = x == 5
-     print("x=", x)
-     print("y=", y)
-     print("z=", z)
-     if y:
-         print("Star Wars Episodes 1,2,3 are the best!")
-     if z:
-         print("Star Wars Episodes 4,5,6 are the best!")
-'''
-
-
-
-
-'''
- 8. Look at the code below. Write you best guess on what it will print.
-     Next, run the code and see if you are correct. 
-     
-     x = 5
-     y = 10
-     z = 10
-     print(x < y)
-     print(y < z)
-     print(x == 5)
-     print(not x == 5)
-     print(x != 5)
-     print(not x != 5)
-     print(x == "5")
-     print(5 == x + 0.00000000001)
-     print(x == 5 and y == 10)
-     print(x == 5 and y == 5)
-     print(x == 5 or y == 5)
-'''
-
-
-
-'''
- 9. Look at the code below. Write you best guess on what it will print.
-     Next, run the code and see if you are correct. (HINT: when comparing strings, ASCII codes are used. https://www.ascii-code.com/)
-     
-     print("3" == "3")
-     print(" 3" == "3")
-     print(3 < 4)
-     print("3" < "4")
-     print("3" < 4)
-     print("<" < ">")
-     print((2 == 2) == "True")
-     print((2 == 2) == True)
-     print("?"<"!")
-'''
-
-
-
-'''
- 10. What things are wrong with this section of code?
-     The programmer wants to set the force sensitivity variable according to the character the user selects.
-     
      print("Welcome to the Jedi Academy!")
 
      print("A. Jedi Master")
@@ -147,10 +48,9 @@ but I will definitely not ask another person except the instructor. Signed: ____
      user_input = input("Choose a character?")
 
      if user_input = A:
-         money = 1000
+         sensitivity = 1000
      else if user_input = B:
-         money = 900
+         sensitivity = 900
      else if user_input = C:
-         money = 0
-'''
+         sensitivity = 0
 
