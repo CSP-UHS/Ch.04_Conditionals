037596cde83ff2884ab4f42f7c912c8c00944dc2
Hibago <45212300+Hibago@users.noreply.github.com>
2019-03-12 11:09:25 -0500

    Update 4.0_Take_Home_Test.py

--- 4.0_Take_Home_Test.py
+++ 4.0_Take_Home_Test.py
@@ -1,51 +1,56 @@
 '''
-HONOR CODE: I solemnly promise that while taking this test I will only use PyCharm or the Internet,
-but I will definitely not ask another person except the instructor. Signed: Varun Vepa
+HONOR CODE: I solemnly promise that while taking this test I will only use PyCharm or the Internet, 
+but I will definitely not ask another person except the instructor. Signed: ______________________
 '''
 
-# 1. Make the following program work.
+  #1. Make the following program work.
+     
+     midichlorians = float(input("Enter midichlorian count: ")
+     if midichlorians > 10000
+         print("You have serious Jedi potential")
+     elif:
+         print("Jedi, you will never be.")
 
-midichlorians = float(input("Enter midichlorian count: ")
-if midichlorians > 10000:
- print("You have serious Jedi potential")
-elif:
- print("Jedi, you will never be.")
 
-# 2. Make the following program work.
+ # 2. Make the following program work.
+     
+     x = input("Enter a number: ")
+     if x = 3
+         print("You entered 3")
 
-x = input("Enter a number: ")
-if x == 3:
- print("You entered 3")
 
-# 3. Make the following program work.
+  # 3. Make the following program work. 
+     
+     answer = input("What is the name of Poe Dameron's Droid? ")
+     if a = "BB8":
+         print("Correct!")
+         else
+         print("Incorrect! It is BB8.")
 
-answer = input("What is the name of Poe Dameron's Droid? ")
-if answer == "BB8":
- print("Correct!")
-else:
- print("Incorrect! It is BB8.")
 
-# 4. Make the following program work.
+  # 4. Make the following program work.
+     
+     x = input("Name one of the top 3 greatest Jedi.")
+     if jedi == Yoda or Luke Skywalker or Obi-Wan Kenobi:
+         print "That is correct!"
 
-jedi = input("Name one of the top 3 greatest Jedi.")
-if jedi == "Yoda" or "Luke Skywalker" or "Obi-Wan Kenobi":
-print("That is correct!")
 
-# 5. Make the following program work whether they enter a, A, Jedi Master or jedi master
 
-print("Welcome to the Jedi Academy!")
+ # 5. Make the following program work whether they enter a, A, Jedi Master or jedi master
+     
+     print("Welcome to the Jedi Academy!")
 
-print("A. Jedi Master")
-print("B. Sith Lord")
-print("C. Droid")
+     print("A. Jedi Master")
+     print("B. Sith Lord")
+     print("C. Droid")
 
-user_input = input("Choose a character?")
+     user_input = input("Choose a character?")
 
-if user_input.lower() == "A" or user_input.lower() == "Jedi Master":
-sensitivity = 1000
-else if user_input = B:
-    sensitivity = 900
-else if user_input = C:
-    sensitivity = 0
+     if user_input = A:
+         sensitivity = 1000
+     else if user_input = B:
+         sensitivity = 900
+     else if user_input = C:
+         sensitivity = 0
 
-print("Sensitivity: ", sensitivity)
+     print("Sensitivity: ",Sensitivity)
