27e08e28ba3c5bdd564dcc70b15843dff5e79125
Hibago <45212300+Hibago@users.noreply.github.com>
2019-03-24 15:31:42 -0500

    Update 4.0_Take_Home_Test.py

--- 4.0_Take_Home_Test.py
+++ 4.0_Take_Home_Test.py
@@ -1,6 +1,6 @@
 '''
 HONOR CODE: I solemnly promise that while taking this test I will only use PyCharm or the Internet,
-but I will definitely not ask another person except the instructor. Signed: ______________________
+but I will definitely not ask another person except the instructor. Signed: Varun Vepa
 '''
 
 # 1. Make the following program work.
