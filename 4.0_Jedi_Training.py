55b53e37d0a27e2a9b6b607c1f7052b5512b0f4a
Marc Hermon <hermonm@urbandaleschools.com>
2019-05-14 15:16:36 -0500

    Update and rename 4.0_Take_Home_Test.py to 4.0_Jedi_Training.py

--- 4.0_Jedi_Training.py
+++ 4.0_Jedi_Training.py
@@ -1,7 +1,4 @@
-'''
-HONOR CODE: I solemnly promise that while taking this test I will only use PyCharm or the Internet, 
-but I will definitely not ask another person except the instructor. Signed: ______________________
-'''
+# Sign your name:________________
 
   #1. Make the following program work.
      
