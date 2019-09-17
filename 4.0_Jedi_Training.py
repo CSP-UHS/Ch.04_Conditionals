ee67e27bb958787e4503b219a815bc2a402aa368
leavertonnel <leavertonnel@urbandaleschools.com>
2019-09-17 14:23:11 -0500

    just a commit

--- 4.0_Jedi_Training.py
+++ 4.0_Jedi_Training.py
@@ -9,13 +9,13 @@
 #     print("Jedi, you will never be.")
 
 
- # 2. Make the following program work.
+# 2. Make the following program work.
      
 # x = input("Enter a number: ")
 # if x : 3
 # print("You entered 3")
 
- #
+
  #  # 3. Make the following program work.
 
 # answer = input("What is the name of Poe Dameron's Droid? ")
@@ -24,30 +24,29 @@
 # else:
 #     print("Incorrect! It is BB8.")
 
- #
+
  #  # 4. Make the following program work.
- #
+
 # jedi = input("Name one of the top 3 greatest Jedi.")
 # if jedi == "Yoda" or "Luke Skywalker" or "Obi-Wan Kenobi":
 #     print ("That is correct!")
 
- #
- #
+
  # # 5. Make the following program work whether they enter a, A, Jedi Master or jedi master
- #
- #     print("Welcome to the Jedi Academy!")
- #
- #     print("A. Jedi Master")
- #     print("B. Sith Lord")
- #     print("C. Droid")
- #
- #     user_input = input("Choose a character?")
- #
- #     if user_input = A:
- #         sensitivity = 1000
- #     else if user_input = B:
- #         sensitivity = 900
- #     else if user_input = C:
- #         sensitivity = 0
- #
- #     print("Sensitivity: ",Sensitivity)
+
+print("Welcome to the Jedi Academy!")
+
+print("A. Jedi Master")
+print("B. Sith Lord")
+print("C. Droid")
+
+user_input = input("Choose a character?")
+
+if user_input == A:
+    sensitivity = 1000
+elif user_input == B:
+    sensitivity = 900
+elif user_input == C:
+    sensitivity = 0
+
+print("Sensitivity: ",Sensitivity)
