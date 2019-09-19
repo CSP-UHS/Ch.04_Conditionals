301757de7ea93759f58a4c1d378e16b7aa16301e
leavertonnel <leavertonnel@urbandaleschools.com>
2019-09-19 13:57:21 -0500

    just a commit

--- 4.1_Number_Analysis.py
+++ 4.1_Number_Analysis.py
@@ -18,3 +18,22 @@ Out:  Test 1: Odd
       Test 2: Negative
       Test 3: Exclusive
 '''
+print("Welcome to my NUMBER ANALYSIS PROGRAM!\n")
+print("Give me a number and ill determine if it is Odd, Even, Positive, Negative, Zero and inclusively between -100 and +100 ")
+
+n = int(input("Whats your Number?\n"))
+
+if n%2 == 1:
+    print("Your number is Even")
+else :
+    print("Your number is Odd")
+
+if n == 0:
+    print("Your number is Sero")
+else :
+    print("Your number is not Zero")
+
+if n-abs(n) == 0:
+    print("your number is negative")
+else :
+    print("Your number is positive")
