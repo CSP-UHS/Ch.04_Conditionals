63e22a16d6c2dd5f667b3a28ab320549faa77372
hermoniuscapensis <phamjul@urbandaleschools.com>
2019-09-20 13:50:16 -0500

    ??

--- 4.0_Jedi_Training.py
+++ 4.0_Jedi_Training.py
@@ -1,6 +1,6 @@
 # # Sign your name:________________
 #
-#   #1. Make the following program work.
+#   #1. Make the following program work. (3 mistakes)
 #
 # midichlorians = float(input("Enter midichlorian count: "))
 # if midichlorians > 10000:
@@ -9,14 +9,14 @@
 #     print("Jedi, you will never be.")
 #
 #
-#  # 2. Make the following program work.
+#  # 2. Make the following program work. (3 mistakes)
 #
 # x = float(input("Enter a number: "))
 # if x == 3:
 #     print("You entered 3")
 #
 #
-#   # 3. Make the following program work.
+#   # 3. Make the following program work. (4 mistakes)
 #
 # answer = input("What is the name of Poe Dameron's Droid? ")
 # if answer == "BB8":
@@ -25,29 +25,33 @@
 #     print("Incorrect! It is BB8.")
 
 
-  # 4. Make the following program work.
+  # 4. Make the following program work. (4 mistakes)
 
-jedi = input("Name one of the top 3 greatest Jedi.")
-if jedi == "Yoda" or jedi == "Luke Skywalker" or jedi == "Obi-Wan Kenobi":
-    print("That is correct!")
+# jedi = input("Name one of the top 3 greatest Jedi.")
+# if jedi == "Yoda" or jedi == "Luke Skywalker" or jedi == "Obi-Wan Kenobi":
+#     print("That is correct!")
 
 
 
- # # 5. Make the following program work whether they enter a, A, Jedi Master or jedi master
- #
- #     print("Welcome to the Jedi Academy!")
- #
- #     print("A. Jedi Master")
- #     print("B. Sith Lord")
- #     print("C. Droid")
- #
- #     user_input = input("Choose a character?")
- #
- #     if user_input = A:
- #         sensitivity = 1000
- #     else if user_input = B:
- #         sensitivity = 900
- #     else if user_input = C:
- #         sensitivity = 0
- #
- #     print("Sensitivity: ",Sensitivity)
+ # 5. Make the following program work whether they enter a, A, Jedi Master or jedi master
+ # Print "Not a choice!" if they don't choose any of the three and set sensitivity to blank text.
+
+print("Welcome to the Jedi Academy!")
+
+print("A. Jedi Master")
+print("B. Sith Lord")
+print("C. Droid")
+
+user_input = input("Choose a character?")
+
+
+if user_input == A:
+    sensitivity = 1000
+elif user_input == B:
+    sensitivity = 900
+elif user_input == C:
+    sensitivity = 0
+elif user_input == 
+    sensitivity = "0"
+
+print("Sensitivity: ",sensitivity)
