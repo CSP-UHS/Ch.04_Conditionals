87c83654c104348cddcae7d7a2a4885115601ae1
halsteaddan <halsteaddan@urbandaleschools.com>
2019-09-23 14:16:53 -0500

    Testing.

--- Testing.py
+++ Testing.py
@@ -1,8 +1,10 @@
 #Conditionalls
 
-jedimaster=input("Name a great jedi master")
+jedimaster=input("Name a great jedi master: ")
 
-if jedimaster=='Luke Skywalker':
+if jedimaster.lower()=='luke skywalker' or jedimaster.lower()=='luke':
     print("Luke is a good choice")
 elif jedimaster=="BB8":
     print("Hey genius BB8 is a DROID!!!")
+else:
+    print("anything else")
