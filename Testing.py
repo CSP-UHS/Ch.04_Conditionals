3c8063d6f26bc4fe3b60dbfb4dda86765d857c36
halsteaddan <halsteaddan@urbandaleschools.com>
2019-09-23 14:08:17 -0500

    Testing.

--- Testing.py
+++ Testing.py
@@ -1,8 +1,8 @@
 #Conditionalls
 
-a=6
-b=5
-if a<b:
-    print("a is less than b")
-else:
-    print("a is bigger than b")
+jedimaster=input("Name a great jedi master")
+
+if jedimaster=='Luke Skywalker':
+    print("Luke is a good choice")
+elif jedimaster=="BB8":
+    print("Hey genius BB8 is a DROID!!!")
