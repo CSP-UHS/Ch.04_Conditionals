2c1e1dd4971b3ed21800303a00f1fa951ba16e77
alexanderrow <alexanderrow@urbandaleschools.com>
2019-09-27 14:20:36 -0500

    some finished code

--- 4.0_Jedi_Training.py
+++ 4.0_Jedi_Training.py
@@ -1,9 +1,58 @@
-# Sign your name:Rowdy Alexander
+# Sign your name: Rowdy Alexander
 
   #1. Make the following program work. (3 mistakes)
 
 midichlorians = float(input("Enter midichlorian count: "))
- if midichlorians >10000:
-    print("You have serious Jedi Potential")
-elif:
+if midichlorians > 10000:
+    print("You have serious Jedi potential")
+else:
     print("Jedi, you will never be.")
+
+
+ # 2. Make the following program work. (3 mistakes)
+
+x=float(input("Enter a number: "))
+if x == 3:
+    print("You entered 3")
+else:
+    print("x is not equal to 3")
+
+
+  # 3. Make the following program work. (4 mistakes)
+
+answer =str(input("What is the name of Poe Dameron's Droid? "))
+if answer == "BB8":
+    print("Correct!")
+else:
+    print("Incorrect! It is BB8.")
+
+
+  # 4. Make the following program work. (4 mistakes)
+
+x = str(input("Name one of the top 3 greatest Jedi."))
+if x == "Yoda" or x == "Luke Skywalker" or x == "Obi-Wan Kenobi:":
+    print("That is correct!")
+else:
+    print("Jedi This is not!")
+
+
+
+ # 5. Make the following program work whether they enter a, A, Jedi Master or jedi master
+ #    Print "Not a choice!" if they don't choose any of the three and set sensitivity to blank text.
+
+     print("Welcome to the Jedi Academy!")
+
+     print("A. Jedi Master")
+     print("B. Sith Lord")              #this is where i left off
+     print("C. Droid")
+
+     user_input = input("Choose a character?")
+
+     if user_input = A:
+         sensitivity = 1000
+     else if user_input = B:
+         sensitivity = 900
+     else if user_input = C:
+         sensitivity = 0
+
+     print("Sensitivity: ",Sensitivity)
