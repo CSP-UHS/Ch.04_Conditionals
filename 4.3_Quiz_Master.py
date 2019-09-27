b01f3ecd99e23f7dcf198877103fc2b7ae3677ab
Tristan Holman <54453352+Scertskrt@users.noreply.github.com>
2019-09-27 13:51:42 -0500

    Finished Quiz Master
    Tristan AKA let me take physics next year

--- 4.3_Quiz_Master.py
+++ 4.3_Quiz_Master.py
@@ -4,3 +4,147 @@ QUIZ MASTER PROJECT
 The criteria for the project are on the website. Make sure you test this quiz with 
 two of your student colleagues before you run it by your instructor.
 '''
+c = 0
+w = 0
+
+print("        Welcome to the Minecraft quiz!")
+print("Enter the right answer to prove you are an")
+print("                    EPIC GAMER                ")
+print("               Ready? Let's start")
+print()
+print("What was the original name of the game?")
+print()
+print("A.  Cave Game")
+print("B.  Craft and Mine")
+print("C.  Square Spelunking")
+print()
+a = str(input("Your answer: "))
+if a.lower() == ("a") or a.lower() == ("cave game"):
+    print()
+    print("Correct!")
+    c+=1
+else:
+    print()
+    print("Incorrect")
+print()
+print("When was the game released?")
+print()
+print("A.  2011")
+print("B.  2005")
+print("C.  2007")
+print()
+a = str(input("Your answer: "))
+if a.lower() == ("a") or a.lower() == ("2011"):
+    print()
+    print("Correct!")
+    c+=1
+else:
+    print()
+    print("Incorrect")
+    w += 1
+print()
+print("How many dyes are in the game?")
+print()
+a = str(input("Your answer: "))
+if a.upper() == "20":
+    print()
+    print("Correct!")
+    c+=1
+else:
+    print()
+    print("Incorrect")
+    w += 1
+print()
+print("What is the rarest ore to get while mining?")
+a = str(input("Your answer: "))
+if a.lower() == ("emeralds") or a.lower() == ("emerald ore") or a.lower() == ("emerald"):
+    print()
+    print("Correct!")
+    c+=1
+else:
+    print()
+    print("Incorrect")
+    w += 1
+print()
+print("What is the final boss of the game?")
+print()
+print("A.  The Wither")
+print("B.  Steve")
+print("C.  The Ender Dragon")
+print()
+a = str(input("Your answer: "))
+if a.lower() == ("c") or a.lower() == ("the ender dragon"):
+    print()
+    print("Correct!")
+    c+=1
+else:
+    print()
+    print("Incorrect")
+    w += 1
+print()
+print("What is the best food source in the game?")
+print()
+print("A.  Golden Apples")
+print("B.  Golden Carrots")
+print("C.  Steak")
+print()
+a = str(input("Your answer: "))
+if a.lower() == ("b") or a.lower() == ("golden carrots"):
+    print()
+    print("Correct!")
+    c+=1
+else:
+    print()
+    print("Incorrect")
+    w += 1
+print()
+print("Which portal requires obsidian to work?")
+print()
+a = str(input("Your answer: "))
+if a.lower() == ("the nether portal") or a.lower() == ("nether") or a.lower() == ("the nether") or a.lower() == ("nether portal"):
+    print()
+    print("Correct!")
+    c+=1
+else:
+    print()
+    print("Incorrect")
+    w += 1
+print()
+print("What does the observer do?")
+print()
+print("A.  Watches for entity movement")
+print("B.  Watches for block updates")
+print("C.  Watches for redstone signals")
+print()
+a = str(input("Your answer: "))
+if a.lower() == ("b") or a.lower() == ("watches for block updates"):
+    print()
+    print("Correct!")
+    c+=1
+else:
+    print()
+    print("Incorrect")
+    w+= 1
+
+grade = 100*(c/(c+w))
+grade//=1
+print("Your score is: ",grade,"%")
+
+if grade == 80:
+    print("You are a true")
+    print(" EPIC GAMER")
+elif grade > 80 and grade < 100:
+    print("You are close, wise one.")
+else:
+    print()
+    print()
+    print()
+    print("             You     died!")
+    print()
+    print()
+    print("               Score: 0")
+    print()
+    print()
+    print("                Respawn")
+    print()
+    print("              Title screen")
