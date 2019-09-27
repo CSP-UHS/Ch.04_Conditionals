c4beb23f465c1c2f8a09e72092d27da8e04c8b33
Tristan Holman <54453352+Scertskrt@users.noreply.github.com>
2019-09-27 13:54:52 -0500

    Finished Jedi Training
    Tristan AKA Let me do physics next year

--- 4.0_Jedi_Training.py
+++ 4.0_Jedi_Training.py
@@ -1,53 +1,61 @@
-# Sign your name:________________
+# Sign your name: Tristan
 
   #1. Make the following program work.
-     
-     midichlorians = float(input("Enter midichlorian count: ")
-     if midichlorians > 10000
-         print("You have serious Jedi potential")
-     elif:
-         print("Jedi, you will never be.")
+
+midichlorians = float(input("Enter midichlorian count: "))
+if midichlorians > 10000:
+    print("You have serious Jedi potential")
+else:
+    print("Jedi, you will never be.")
 
 
- # 2. Make the following program work.
-     
-     x = input("Enter a number: ")
-     if x = 3
-         print("You entered 3")
+ # 2. Make the following program work. (3 mistakes)
+
+x = float(input("Enter a number: "))
+if x == 3:
+    print("You entered 3")
+else:
+    print("You did not enter 3")
 
 
-  # 3. Make the following program work. 
-     
-     answer = input("What is the name of Poe Dameron's Droid? ")
-     if a = "BB8":
-         print("Correct!")
-         else
-         print("Incorrect! It is BB8.")
+  # 3. Make the following program work. (4 Mistakes)
+
+answer = str(input("What is the name of Poe Dameron's Droid? "))
+if answer.upper() == ("BB8"):
+    print("Correct!")
+else:
+    print("Incorrect! It is BB8.")
 
 
-  # 4. Make the following program work.
-     
-     x = input("Name one of the top 3 greatest Jedi.")
-     if jedi == Yoda or Luke Skywalker or Obi-Wan Kenobi:
-         print "That is correct!"
+  # 4. Make the following program work. (4 mistakes)
+jedi = str(input("Name one of the top 3 greatest Jedi."))
+if jedi.upper() == ("YODA") or jedi.upper() == ("LUKE SKYWALKER") or jedi.upper() == ("OBI-WAN KENOBI"):
+    print("That is correct!")
+else:
+    print("The force is not with you.")
 
 
 
  # 5. Make the following program work whether they enter a, A, Jedi Master or jedi master
+ # Print "Not a choice!" if they don't choose any of the tree and set sensitivity to blank text.
      
-     print("Welcome to the Jedi Academy!")
+print("Welcome to the Jedi Academy!")
 
-     print("A. Jedi Master")
-     print("B. Sith Lord")
-     print("C. Droid")
+print("A. Jedi Master")
+print("B. Sith Lord")
+print("C. Droid")
 
-     user_input = input("Choose a character?")
+user_input = input("Choose a character? ")
 
-     if user_input = A:
-         sensitivity = 1000
-     else if user_input = B:
-         sensitivity = 900
-     else if user_input = C:
-         sensitivity = 0
+if user_input.upper() == ("A") or user_input.upper() == ("JEDI MASTER") or user_input.upper() == ("A. JEDI MASTER"):
+    sensitivity = 1000
+elif user_input.upper() == ("B") or user_input.upper() == ("SITH LORD") or user_input.upper() == ("B. SITH LORD") :
+    sensitivity = 900
+elif user_input.upper() == ("C") or user_input.upper() == ("DROID") or user_input.upper() == ("C. DROID"):
+    sensitivity = 0
+else:
+    print("Not a choice!")
+    sensitivity = None
 
-     print("Sensitivity: ",Sensitivity)
+print("Sensitivity: ", sensitivity)
+
