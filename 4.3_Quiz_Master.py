8af503d7a7dfffd0ab0537fcf433b6f0f9fd5a38
watsoncal <watsoncal@urbandaleschools.com>
2019-09-27 14:19:43 -0500

    Questions

--- 4.3_Quiz_Master.py
+++ 4.3_Quiz_Master.py
@@ -4,3 +4,26 @@ QUIZ MASTER PROJECT
 The criteria for the project are on the website. Make sure you test this quiz with 
 two of your student colleagues before you run it by your instructor.
 '''
+
+print("Welcome to my Shrek Quiz!")
+
+print("Question #1\nWhat year was the original Shrek movie released?")
+
+print("Question #2\nHow many Shrek movies have been released?")
+print("A. 4\nB. 3\nC. 1\nD. 2")
+
+print("Question #3\nFinish the quote: What are you doing in my _____?")
+
+print("Question #4\nWhat does Gingy insist that Lord Farquaad not touch?")
+print("A. his gumdrop eyes\nB. his heart\nC. his eyes\nD. his gumdrop buttons")
+
+print("Question #4\nWhat character(s) turns into a frog before their death?")
+print("A. King Harold\nB. Lord Farquaad\nC. Gingy\nD. all of the above")
+
+print("Question #5\nTrue or False: The Fairy Godmother is Prince Charming's grandma.")
+
+print("Question #6\nIn what language does Shrek mean monster?")
+print("A. Italian\nB. Armenian\nC. Yiddish\nD. Swahili")
+
+print("Question #7\nWhich of the following is Cal's favorite Shrek movie?")
+print("A. Shrek\nB. Shrek the Third\nC. Shrek Forever After\nD. Shrek 2")
