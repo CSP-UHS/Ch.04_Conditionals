c67ff23c21b7afc460f360a4992ddcb35b358713
halsteaddan <halsteaddan@urbandaleschools.com>
2019-10-03 14:00:27 -0500

    Quizmaster Stuff.

--- 4.3_Quiz_Master.py
+++ 4.3_Quiz_Master.py
@@ -7,7 +7,6 @@ two of your student colleagues before you run it by your instructor.
 
 print ("Thanks for taking my quiz about Minecraft.")
 print("This won't be easy, only Minecraft pros will get a perfect score.")
-
 NumberOfQuestionsAnswered = 0
 NumberOfQuestionsCorrect = 0
 
@@ -35,9 +34,46 @@ UserInput = str(input("Choose an answer: "))
 NumberOfQuestionsAnswered + 1
 if UserInput.lower() == "creeper" or UserInput.lower() == "charged creeper":
     NumberOfQuestionsCorrect + 1
-    print("Good job..")
+    print("Good job.")
 else:
     print("Wrong, the right answer was Creeper or Charged Creeper.")
+
+print("How many different ores are in Minecraft? (count the two technically different kinds of gold a one).")
+UserInput = str(input("Choose an answer: "))
+NumberOfQuestionsAnswered + 1
+if UserInput == "7" or UserInput.lower() == "seven":
+    NumberOfQuestionsCorrect + 1
+    print("Good job.")
+else:
+    print("Wrong, the right answer was seven.")
+
+print("True or False, all platforms where Minecraft has been released are able to do cross platform play.")
+UserInput = str(input("Choose an answer: "))
+NumberOfQuestionsAnswered + 1
+if UserInput.lower() == "false" or UserInput.lower() == "no":
+    NumberOfQuestionsCorrect + 1
+    print("Good job.")
+else:
+    print("Wrong, While pc, xbox, and mobile are able to do cross platform play, it is not availible on ps4, old gen console, ps vita, rasberry pie, 3ds, wii u. .")
+
+print("How much does a three player realm cost for a one month subscription on Minecraft bedrock edition?")
+print("Do not include tax.")
+UserInput = str(input("Choose an answer: $"))
+NumberOfQuestionsAnswered + 1
+if UserInput == "3.99" or UserInput == "4":
+    NumberOfQuestionsCorrect + 1
+    print("Good job.")
+else:
+    print("Wrong, the right answer was $3.99.")
+
+print("Which java edition mod improves performance, adds dynamic lighting, and allows all players using that mod to see each others capes?")
+UserInput = str(input("Choose an answer: "))
+NumberOfQuestionsAnswered + 1
+if UserInput.lower() == "optifine":
+    NumberOfQuestionsCorrect + 1
+    print("Good job.")
+else:
+    print("Wrong, the right answer was Optifine.")
 
 
 
