75f4afb7f6019526662a89596c3c99afbc9c0db2
calebhews <calebhews@gmail.com>
2019-10-03 13:35:26 -0500

    Chapter 3

--- 4.2_Grading_2.0.py
+++ 4.2_Grading_2.0.py
@@ -4,3 +4,25 @@ GRADING 2.0
 Copy your Grading 1.0 program and modify it to also print out the letter grade depending on the numerical grade.
 If they fail, tell them to "Transfer to Johnston!"
 '''
+print("Would you like to know how your grades will end up? Look no further than right here!")
+
+
+sgrade=int(input("What is your Semester Grade?"))
+fgrade=int(input("What is your Final Grade?"))
+exworth=int(input("What is your Exam worth?"))
+exworth/=100
+
+overall=sgrade*(1-exworth)+(fgrade*exworth)
+
+print("Your Overall grade is",overall)
+
+if overall>=90.0:
+    print("Your grade is an A")
+elif overall>80.0:
+    print("Your grade is a B")
+elif overall>70.0:
+    print("Your grade is a C")
+elif overall>60.0:
+    print("Your grade is a D")
+else:
+    print("Your grade is an F. Transfer to Johnston!")
---
+++ testing.py
@@ -0,0 +1,21 @@
+
+
+'''
+jedimaster=input("Name a great jedi master: ")
+
+if jedimaster.lower() == "luke skywalker" or jedimaster.lower() == "luke":
+    print("Luke is a good choice!")
+elif jedimaster =="BB8":
+    print("Hey idiot, BB-8 is a DROID!")
+else:
+    print("anything else")
+'''
+'''
+game_over = False
+
+if game_over:
+    print("The game is over! You Lose!")
+else:
+    print("The game is still running!")
+'''
+#print("yay\ngood")
