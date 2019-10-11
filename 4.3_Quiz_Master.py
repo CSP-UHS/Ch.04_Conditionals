90fdd81424c6eecc267717346906eff51ff0fe1a
cobbzac <cobbzac@urbandaleschools.com>
2019-10-11 14:23:25 -0500

    chp.4 test started

--- 4.3_Quiz_Master.py
+++ 4.3_Quiz_Master.py
@@ -4,3 +4,15 @@ QUIZ MASTER PROJECT
 The criteria for the project are on the website. Make sure you test this quiz with 
 two of your student colleagues before you run it by your instructor.
 '''
+print("welcome to my quzi about random things")
+ans = input("are you ready to begin?")
+if ans ==(str("yes"))or("Yes"):
+    print("have fun all your data will be stolen and sold to the highest bidder")
+
+an = int(input("how moons does pluto have?"))
+if an ==5:
+    print("corret")
+else:
+    print("incorrect pluto has 5 moons")
+
+
---
+++ test.py
@@ -0,0 +1,19 @@
+poly = int(input("how many sides do you want in your polygon?"))
+import turtle
+tina = turtle
+tina.shape
+angle = 360/poly
+for i in range(poly):
+    tina.forward(50)
+    tina.left(angle)
+
+
+
+
+
+
+
+
+
+
+
