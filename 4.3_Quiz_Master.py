ebea3b2e02c42af9737abbcae05df243cf0fdf71
cobbzac <cobbzac@urbandaleschools.com>
2019-10-15 14:21:13 -0500

    6 questions done the beginning still doesn't work

--- 4.3_Quiz_Master.py
+++ 4.3_Quiz_Master.py
@@ -5,14 +5,47 @@ The criteria for the project are on the website. Make sure you test this quiz wi
 two of your student colleagues before you run it by your instructor.
 '''
 print("welcome to my quzi about random things")
-ans = input("are you ready to begin?")
-if ans ==(str("yes"))or("Yes"):
+ans = input("are you ready to begin?""        a. yes"
+            "       b. no")
+if ans == (str("a"))or("A"):
     print("have fun all your data will be stolen and sold to the highest bidder")
+else:
+
+
 
 an = int(input("how moons does pluto have?"))
 if an ==5:
     print("corret")
 else:
     print("incorrect pluto has 5 moons")
+a = input("Which Graphics card is better?"
+          "       a. GeForce RTX 2060"
+          "       b. Radeon R9 380")
+if a==(str("a")) or (str("A")):
+    print("correct")
+else:
+    print("incorrect")
+b = input("How many holes are in a straw?"
+          "    a. 2"
+          "    b. 1")
+if b == str("b") or ("B"):
+    print("correct")
+else:
+    print("that is incorrect there is only one hole in a straw.")
+c = input("Who won super bowl 21?"
+          "      a.New England Patriots"
+          "      b.New York Jets"
+          "      c.New York Giants"
+          "      d.Minnesota Vikings")
+if c ==str("c") or (str("C")):
+    print("correct! They beat the Denver Broncos 39-20")
+else:
+    print("NOPE")
+d = input("What is your name?")
+print("Imagine carrying around in your stomach for 9 months just to name it",d)
+
+
+
+
 
 
