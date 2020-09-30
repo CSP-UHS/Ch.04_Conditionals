987367551ecfeed4e104c81ae27a6acff5192f4a
mearsale <alexmears2003@gmail.com>
2020-09-30 10:35:28 -0500

    Did the thing

--- 4.0_Jedi_Training.py
+++ 4.0_Jedi_Training.py
@@ -1,36 +1,49 @@
 # Sign your name:________________
 
   #1. Make the following program work. (3 mistakes)
-     
+'''
      midichlorians = float(input("Enter midichlorian count: ")
      if midichlorians > 10000
          print("You have serious Jedi potential")
      elif:
          print("Jedi, you will never be.")
-
+'''
+midichlorians = float(input("Enter midichlorian count: "))  # Finish parenthesis
+if midichlorians > 10000:                                   # Add :
+    print("You have serious Jedi potential")
+else:                                                       # Change elif to else
+    print("Jedi, you will never be.")
 
  # 2. Make the following program work. (3 mistakes)
-     
+'''
      x = input("Enter a number: ")
      if x = 3
          print("You entered 3")
-
+'''
+x = int(input("Enter a number: "))  # Added int to make it recognize answer as number
+if x == 3:                          # Added an = and : for proper syntax
+    print("You entered 3")
 
   # 3. Make the following program work. (4 mistakes)
-     
+'''
      answer = input("What is the name of Poe Dameron's Droid? ")
      if a = "BB8":
          print("Correct!")
          else
          print("Incorrect! It is BB8.")
-
+'''
+answer = input("What is the name of Poe Dameron's Droid? ")
+if answer == "BB8":                                             # Changed var to answer and added =
+    print("Correct!")
+else:                                                           # Removed Indent and added :
+    print("Incorrect! It is BB8.")
 
   # 4. Make the following program work. (4 mistakes)
-     
+'''
      x = input("Name one of the top 3 greatest Jedi.")
      if jedi == Yoda or Luke Skywalker or Obi-Wan Kenobi:
          print "That is correct!"
-
+'''
 
 
  # 5. Make the following program work whether they enter a, A, Jedi Master or jedi master
---
+++ Practice_CH4.py
@@ -0,0 +1,3 @@
+jedi = input("Name one of the top 3 greatest Jedi.")
+if jedi == "Yoda" or Luke Skywalker or Obi-Wan Kenobi:
+    print "That is correct!"
