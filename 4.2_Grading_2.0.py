551555584127ac258ba3a0524d3d3be74fe2c01f
redeniuseva <redeniuseva@urbandaleschools.com>
2020-09-30 12:06:07 -0500

    Fin Grading

--- 4.2_Grading_2.0.py
+++ 4.2_Grading_2.0.py
@@ -5,6 +5,36 @@ Copy your Grading 1.0 program and modify it to also print out the letter grade d
 If they fail, tell them to "Transfer to Johnston!"
 '''
 
-a=4
-b=5
-if a<b:
+
+print()
+semester=int(input("What is your current semester grade: "))
+Test=int(input("What is your Test Grade: "))
+Worth=int(input("What is the Test worth: "))
+Worth=(Worth/100)
+Final=semester*(1-Worth) + Test*(Worth)
+print()
+if Final>=93:
+    print("you got an A Great Job!")
+elif Final>=90:
+    print("You got an A- Great Job.")
+elif Final>=87:
+    print("you got a B+ Great Job.")
+elif Final>=83:
+    print("you got a B Great.")
+elif Final>=80:
+    print("you got a B- Great.")
+elif Final>=77:
+    print("you got a C+ Might wanna retake")
+elif Final>=73:
+    print("you got a C Might wanna retake")
+elif Final>=70:
+    print("you got a C- Might wanna retake")
+elif Final>=67:
+    print("you got a D+ your gonna need to retake")
+elif Final>=63:
+    print("you got a D your gonna need to retake")
+elif Final>=60:
+    print("you got a D- your gonna need to retake")
+elif Final<60:
+    print("you got a F you better transfer to Johnston!")
+print("This is your final grade",Final)
