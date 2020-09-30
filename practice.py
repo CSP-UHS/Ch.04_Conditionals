71328b8b99ed6470123a98faf55bc7c40ceb1b9c
lopezsantiagoger <lopezsantiagoger@urbandaleschools.com>
2020-09-30 10:24:09 -0500

    amd or intel

---
+++ practice.py
@@ -0,0 +1,8 @@
+cpu=input("AMD or Intel ")
+
+if cpu=="AMD":
+    print("You are truly part of the PC Master Race!")
+elif cpu=="Intel":
+    print("Still part of the PC Master Race but choose AMD next time")
+else:
+    print("What kind of CPU are you using bro?")
