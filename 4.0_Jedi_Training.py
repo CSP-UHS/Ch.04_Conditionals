6cb06605505c1f858b746a673075c82c90a8dfef
Geni W <williamsgen@urbandaleschools.com>
2020-10-01 21:51:57 -0500

    jedi training

--- 4.0_Jedi_Training.py
+++ 4.0_Jedi_Training.py
@@ -10,44 +10,47 @@ else:
 
  # 2. Make the following program work. (3 mistakes)
      
-x = input("Enter a number: ")
-if x = 3
-print("You entered 3")
+x = int(input("Enter a number: "))
+if x == 3:
+    print("You entered 3")
 
 
   # 3. Make the following program work. (4 mistakes)
 
 answer = input("What is the name of Poe Dameron's Droid? ")
-if a = "BB8":
+if answer == "BB8":
     print("Correct!")
-else
+else:
     print("Incorrect! It is BB8.")
 
 
   # 4. Make the following program work. (4 mistakes)
      
-x = input("Name one of the top 3 greatest Jedi.")
-if jedi == Yoda or Luke Skywalker or Obi-Wan Kenobi:
-    print "That is correct!"
+jedi = input("Name one of the top 3 greatest Jedi.")
+if jedi == "Yoda" or jedi == "Luke Skywalker" or jedi == "Obi-Wan Kenobi":
+    print("That is correct!")
 
 
 
  # 5. Make the following program work whether they enter a, A, Jedi Master or jedi master
  #    Print "Not a choice!" if they don't choose any of the three and set sensitivity to blank text.
      
-     print("Welcome to the Jedi Academy!")
+print("Welcome to the Jedi Academy!")
 
-     print("A. Jedi Master")
-     print("B. Sith Lord")
-     print("C. Droid")
+print("A. Jedi Master")
+print("B. Sith Lord")
+print("C. Droid")
 
-     user_input = input("Choose a character?")
+user_input = input("Choose a character. ")
 
-     if user_input = A:
-         sensitivity = 1000
-     else if user_input = B:
-         sensitivity = 900
-     else if user_input = C:
-         sensitivity = 0
+if user_input.lower() == "a" or user_input.lower() == "a. jedi master" or user_input.lower() == "jedi master" or user_input.lower() == "a jedi master":
+    sensitivity = 1000
+elif user_input.lower() == "b" or user_input.lower() == "b. sith lord" or user_input.lower() == "sith lord" or user_input.lower() == "b sith lord":
+    sensitivity = 900
+elif user_input.lower() == "c" or user_input.lower() == "c. droid" or user_input.lower() == "droid" or user_input.lower() == "c droid":
+    sensitivity = 0
+else:
+    print("Not a choice!")
+    sensitivity = ""
 
-     print("Sensitivity: ",Sensitivity)
+print("Sensitivity: ",sensitivity,)
--- Practice.py
+++ Practice.py
@@ -1,4 +1,19 @@
-jedi=input("Enter a Jedi Master: ")
+print("Welcome to the Jedi Academy!")
 
-if jedi.lower()=="yoda":
-    print("Jedi Master, he is!")
+print("A. Jedi Master")
+print("B. Sith Lord")
+print("C. Droid")
+
+user_input = input("Choose a character. ")
+
+if user_input.lower() == "a" or user_input.lower() == "a. jedi master" or user_input.lower() == "jedi master" or user_input.lower() == "a jedi master":
+    sensitivity = 1000
+elif user_input.lower() == "b" or user_input.lower() == "b. sith lord" or user_input.lower() == "sith lord" or user_input.lower() == "b sith lord":
+    sensitivity = 900
+elif user_input.lower() == "c" or user_input.lower() == "c. droid" or user_input.lower() == "droid" or user_input.lower() == "c droid":
+    sensitivity = 0
+else:
+    print("Not a choice!")
+    sensitivity = ""
+
+print("Sensitivity: ",sensitivity,)
