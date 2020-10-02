0b721216b38a8ba0967e4532443ec5e429abdf94
Geni W <williamsgen@urbandaleschools.com>
2020-10-01 22:17:00 -0500

    number analysis & grading 2.0

--- 4.1_Number_Analysis.py
+++ 4.1_Number_Analysis.py
@@ -18,3 +18,20 @@ Out:  Test 1: Odd
       Test 2: Negative
       Test 3: Exclusive
 '''
+
+number = int(input("Please enter a number: "))
+if (number % 2) == 0:
+    print("Test 1:",number, "is Even.")
+else:
+    print("Test 1:",number, "is Odd.")
+
+if number > 0:
+    print("Test 2:",number, "is Positive.")
+elif number < 0:
+    print("Test 2:",number, "is Negative.")
+else:
+    print("Test 2:",number, "equals 0.")
+if number<101 and number>-101:
+    print("Test 3:",number, "is Inclusive.")
+else:
+    print("Test 3:",number, "is Exclusive.")
--- 4.2_Grading_2.0.py
+++ 4.2_Grading_2.0.py
@@ -4,3 +4,34 @@ GRADING 2.0
 Copy your Grading 1.0 program and modify it to also print out the letter grade depending on the numerical grade.
 If they fail, tell them to "Transfer to Johnston!"
 '''
+
+sem= int(input("Please enter your semester grade: "))
+final= int(input("Please enter your final exam grade: "))
+worth= int(input("Please enter your exam worth: "))
+overall= (sem*((100-worth)/100))+(final*(worth/100))
+if overall > 92:
+    letter = "A"
+elif overall > 87:
+    letter = "A-"
+elif overall > 85:
+    letter = "B+"
+elif overall > 82:
+    letter = "B"
+elif overall > 77:
+    letter = "B-"
+elif overall > 75:
+    letter = "C+"
+elif overall > 72:
+    letter = "C"
+elif overall > 67:
+    letter = "C-"
+elif overall > 65:
+    letter = "D+"
+elif overall > 62:
+    letter = "D"
+elif overall > 50:
+    letter = "D-"
+else:
+    letter = "F"
+    overall = "Transfer to Johnston!"
+print("This is your overall final grade:",letter, "" ,overall,)
