64cf9d53067ad19e983aac1a0849b8d0493cfe4b
dautom21 <dautom@urbandaleschools.com>
2020-10-05 01:31:50 -0500

    4.2 Grading finished

--- 4.2_Grading_2.0.py
+++ 4.2_Grading_2.0.py
@@ -4,12 +4,37 @@ GRADING 2.0
 Copy your Grading 1.0 program and modify it to also print out the letter grade depending on the numerical grade.
 If they fail, tell them to "Transfer to Johnston!"
 '''
-print("I will code this and I will make it laugh at you if you have an overall of 69. Hopefully.")      #please dont dock me points for this
-semgrade=int(input("Give me your semester grade.  "))                                                   #so for the first example thing above
-finalexam=int(input("Give me your final exam grade.  "))                                                #how do i get it to stop at 80.9
-examworth=int(input("Cool. How much is the exam worth?  "))                                             #instead of having it go on for like 20 digits
-examworth=examworth/100                                                                                 #or getting it to stop at 69 instead
-overall=(semgrade*(1-examworth))+(finalexam*examworth)                                                  #of 69.0
-if overall == 69:                                                                                       #also you should give ryan muetzel and daniel mitchell
-    print("Your overall is:",overall,'hahahahahahhahahahahahahaha')                                     #extra points for helping me with the if else statement
-else: (print("Your overall is:",overall,"Congrats, unless you're Ryan Muetzel"))                         #please
+print("I will code this and I will make it laugh at you if you have an overall of 69. Hopefully.")
+
+semgrade=int(input("Give me your semester grade.  "))
+
+finalexam=int(input("Give me your final exam grade.  "))
+
+examworth=int(input("Cool. How much is the exam worth?  "))
+
+examworth=examworth/100
+
+overall=(semgrade*(1-examworth))+(finalexam*examworth)
+
+if overall >= 90:
+    print("A")
+    (print("Your overall is:", overall, "Congrats, unless you're Ryan Muetzel"))                    # i mean
+
+elif overall >= 80:
+    print("B")
+    (print("Your overall is:", overall, "Congrats, unless you're Ryan Muetzel"))                    # it was
+
+elif overall >= 70:
+    print ("C")
+    (print("Your overall is:", overall, "Congrats, unless you're Ryan Muetzel"))                    # simple enough
+
+elif overall >= 60 and overall < 69:
+    print("D")
+    (print("Your overall is:", overall, "Congrats, unless you're Ryan Muetzel"))                    # but i couldnt
+
+elif overall == 69:
+    print("D")
+    print("Your overall is:",overall,"hahahahahahhahahahahahahaha")                                 # ahahahahhahaha teenage brain go brrr
+elif overall < 60:
+    print("F")
+    print("Your overall is:", overall,"Move to Johnston foolish mortal")                            # figure out how to have 1 your overall is ~~~
