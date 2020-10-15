956741c54684374b9363e968b24743d85766eba2
caltrideraar <caltrideraar@urbandaleschools.com>
2020-10-15 10:02:09 -0500

    Finished 4.1

--- 4.1_Number_Analysis.py
+++ 4.1_Number_Analysis.py
@@ -18,3 +18,22 @@ Out:  Test 1: Odd
       Test 2: Negative
       Test 3: Exclusive
 '''
+
+t1 = int(input("Enter your number:"))
+
+if (t1 % 2) == 0:
+    print("Your number is even.")
+else:
+    print("Your number is odd.")
+
+if t1 > 0:
+    print("Your number is positive.")
+if t1 == 0:
+    print("Your number is 0.")
+if t1 < 0:
+    print("Your number is negative.")
+
+if t1 >= -100 and t1 <= 100:
+    print("Your number is inclusive.")
+else:
+    print("Your number is exclusive.")
