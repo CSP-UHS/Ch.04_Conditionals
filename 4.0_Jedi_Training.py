e832feed2d6a87f1a56c6e67936855b5426011e3
thawngbaw <thawngbaw@urbandaleschools.com>
2020-10-21 14:24:24 -0500

    chapter 4 jedi training

--- 4.0_Jedi_Training.py
+++ 4.0_Jedi_Training.py
@@ -1,6 +1,6 @@
 # Sign your name:__Bawi Thawng____
 
-  #1. Make the following program work. (3 mistakes)
+# 1. Make the following program work. (3 mistakes)
      
 midichlorians = float(input("Enter midichlorian count: "))
 if midichlorians > 10000:
@@ -9,10 +9,11 @@ else:
     print("Jedi, you will never be.")
 
 
- # 2. Make the following program work. (3 mistakes)
+# 2. Make the following program work. (3 mistakes)
 x = int(input("Enter a number: "))
 if x == 3:
-print("You entered 3")
+    print("You entered 3")
+
 
 
   # 3. Make the following program work. (4 mistakes)
@@ -20,7 +21,7 @@ print("You entered 3")
 answer = input("What is the name of Poe Dameron's Droid? ")
 if answer == "BB8":
     print("Correct!")
-else :
+else:
     print("Incorrect! It is BB8.")
 
 
@@ -28,12 +29,12 @@ else :
      
 x = input("Name one of the top 3 greatest Jedi.")
 if x.lower() == "yoda" or x.lower() == "luke" "skywalker" or x.lower() == "obi-wan kenobi":
-    print ("That is correct!")
+    print("That is correct!")
 
 
 
- # 5. Make the following program work whether they enter a, A, Jedi Master or jedi master
- #    Print "Not a choice!" if they don't choose any of the three and set sensitivity to blank text.
+# 5. Make the following program work whether they enter a, A, Jedi Master or jedi master
+#    Print "Not a choice!" if they don't choose any of the three and set sensitivity to blank text.
 
 print("Welcome to the Jedi Academy!")
 print("A. Jedi Master")
@@ -53,5 +54,4 @@ elif user_input.lower() == "c" or user_input.lower() == "droid":
     sensitivity = 0
 elif user_input == " ":
     print("not a choice")
-
 print("Sensitivity: ", )
