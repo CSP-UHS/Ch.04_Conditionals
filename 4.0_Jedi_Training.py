d43989ad6d54e3087b05095c4221b440a111aaff
redeniuseva <redeniuseva@urbandaleschools.com>
2020-10-21 09:26:53 -0500

    Finished Jedi trainings

--- 4.0_Jedi_Training.py
+++ 4.0_Jedi_Training.py
@@ -1,18 +1,18 @@
-# Sign your name:________________
+# Sign your name:Evan Redenius
 
   #1. Make the following program work. (3 mistakes)
 
-midichlorians = float(input("Enter midichlorian count: "))
+midichlorians = int(input("Enter midichlorian count: "))
      if midichlorians > 10000:
          print("You have serious Jedi potential")
-     elif:
+     else:
          print("Jedi, you will never be.")
 
 
  # 2. Make the following program work. (3 mistakes)
      
      x = input("Enter a number: ")
-     if x = 3
+     if x = 3:
          print("You entered 3")
 
 
@@ -21,14 +21,14 @@ midichlorians = float(input("Enter midichlorian count: "))
      a = int(input("What is the name of Poe Dameron's Droid? "))
      if a = "BB8":
          print("Correct!")
-         else
+     else:
          print("Incorrect! It is BB8.")
 
 
   # 4. Make the following program work. (4 mistakes)
      
-     x = input("Name one of the top 3 greatest Jedi.")
-     if jedi == Yoda or Luke Skywalker or Obi-Wan Kenobi:
+     jedi = input("Name one of the top 3 greatest Jedi.")
+     if jedi == ("Yoda" or "Luke Skywalker" or "Obi-Wan Kenobi"):
          print ("That is correct!")
 
 
@@ -38,17 +38,17 @@ midichlorians = float(input("Enter midichlorian count: "))
      
      print("Welcome to the Jedi Academy!")
 
-     print("A. Jedi Master")
-     print("B. Sith Lord")
-     print("C. Droid")
+     A= int("A. Jedi Master")
+     B= int(input("B. Sith Lord"))
+     C= int(input("C. Droid"))
 
      user_input = input("Choose a character?")
 
      if user_input = A:
          sensitivity = 1000
-     else if user_input = B:
+     elif user_input = B:
          sensitivity = 900
-     else if user_input = C:
+     elif user_input = C:
          sensitivity = 0
 
-     print("Sensitivity: ",Sensitivity)
+     print("Sensitivity: ",sensitivity)
