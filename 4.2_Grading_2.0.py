a5f0c2861eb017aab99d56297bc1494cdfc1bee1
hmarlal1 <hmarlal1@urbandaleschools.com>
2020-10-29 09:42:46 -0500

    eysyesyesyese

--- 4.2_Grading_2.0.py
+++ 4.2_Grading_2.0.py
@@ -10,12 +10,12 @@ exam_Worth = int(input("Enter the % of your grade the exam is worth:"))
 exam_Worth: float = exam_Worth/100
 overall_Grade = sem_Grade*(1-exam_Worth)+final_Exam*exam_Worth
 if overall_Grade < 65:
-    print('This is your overall grade:', overall_Grade, "which is a F")
+    print("This is your overall grade:", overall_Grade, "which is a F")
 elif overall_Grade >= 90:
-    print('This is your overall grade:', overall_Grade, "which would be an A")
-elif overall_Grade >= 80:
-    print('This is your overall grade:', overall_Grade, "which is a B")
-elif overall_Grade >= 70:
-    print('This is your overall grade:', overall_Grade, "which would be a C")
-elif overall_Grade >= 65:
-    print('This is your overall grade:', overall_Grade, "which is a D")
+    print("This is your overall grade:", overall_Grade, "which would be an A")
+elif overall_Grade >= 80 and overall_Grade < 90:
+    print("This is your overall grade:", overall_Grade, "which is a B")
+elif overall_Grade >= 70 and overall_Grade < 80:
+    print("This is your overall grade:", overall_Grade, "which would be a C")
+elif overall_Grade >= 65 and overall_Grade <70:
+    print("This is your overall grade:", overall_Grade, "which is a D")
