5c4ecaf724092f0e49984fb492926d7b085043fa
hmarlal1 <hmarlal1@urbandaleschools.com>
2020-11-03 00:44:53 -0600

    JUST NEEDS SPACE YEAA!

--- 4.3_Quiz_Master.py
+++ 4.3_Quiz_Master.py
@@ -18,78 +18,107 @@ To create a running total of the number correct, create a variable to store this
 Calculate the percentage by using a formula at the end of the game. Don't just add a certain percentage for each question the user gets correct. If you add a certain percentage each time, then you have to change the program in all of those places if some day you want to add another question. With a formula, you only need 1 change.
 To print a blank line so that all the questions don't run into each other, use the following code:'''
 
-print("Just a quiz with 1 math question mostly about Twitch streamers and Youtubers.")
+print("Welcome to the Slappening Quiz")
 
 score = 0
 
-name = input(print("What is your name? "))
-print(name, "")
-print("")
-print("")
-print("")
-answerONE = input("")
+name = input(print("Enter your name "))
+print(name, "What is the velocity required to cook a chicken with one slap")
+print("A. 3725.95 mph.")
+print("B. Really fast.")
+print("C. 4327.88 m/s.")
+answerONE = input("Answer by entering the corresponding letter that represents the answer: ")
 
 if answerONE.lower() == 'a':
-    print("")
-    score = score+
-    print("", score)
+    print("Correct, how'd you know?")
+    score = score+20
+    print("Your current score is", score)
 elif answerONE.lower() == 'b':
-    print("")
-    score = score+
-    print("", score)
+    print("Incorrect, the right answer was A")
+    score = score+0
+    print("Your current score is", score)
 elif answerONE.lower() == 'c':
-    print("")
-    score = score+
-    print("", score)
+    print("Incorrect, the right answer was A.")
+    score = score+0
+    print("Your current score is", score)
 else:
-    print("")
-    print("", score)
+    print("You have greatly disappointed me.")
+    print("Your current score is", score)
 
-print(name, "")
-print("")
-print("")
-print("")
-answerDOS = input("")
+print(name, "What is the difference between a punch and a slap?")
+print("A. No difference.")
+print("B. When you punch your hand is closed and when you slap it's wide open.")
+print("C. Real man only punch real man.")
+answerDOS = input("Enter your answer: ")
 
 if answerDOS.lower() == 'a':
-    print("")
-    score = score +
-    print("", score)
-elif answerDOS.lower() == 'b':
-    print("")
-    score = score+
-    print("", score)
-elif answerDOS.lower() == 'c':
-    print("")
+    print("Incorrect, the correct answer was B.")
     score = score + 0
-    print("", score)
+    print("Your current score is", score)
+elif answerDOS.lower() == 'b':
+    print("Ez credit bro.")
+    score = score+10
+    print("Your current score is", score)
+elif answerDOS.lower() == 'c':
+    print("Do you think you're still an edgy teenager? Grow up buddy!")
+    score = score + 0
+    print("Your current score is", score)
 else:
-    print("")
-    score = score +
-    print("", score)
+    print("Smooth brain.")
+    score = score +0
+    print("Your current score is", score)
 
-print(name, "")
-answerTRES = input(" ")
+print("How many slaps do YOU think it takes to cook a chicken on average")
+answerTRES = int(input("Enter the amount without commas: "))
 
-if answerTRES.lower() == '' or answerTRES.lower() == '':
-    print("")
-    score = score+
-    print("", score)
+if answerTRES >= 10000:
+    print("Multiple sources state on average around 23034 but, gotta make this test easier.")
+    score = score+10
+    print("Your current score is", score)
 else:
-    print("")
-    print("", score)
+    print("Incorrect, the right answer was at least 10000")
+    print("Your current score is", score)
 
-print(name, "")
-answerCuatro = int(input(""))
+print("Is it possible to die from a sudden slap on the external genitalia?")
+answerCuatro = input("True or false: ")
 
-if answerCuatro == '':
-    print("")
-    score = score+
-    print("", score)
+if answerCuatro.lower() == 'true' or answerCuatro.lower() == 't':
+    print("Well one way or another I guess.")
+    score = score+10
+    print("Correct, your current score is", score)
 else:
-    print("")
-    print("")
+    print("Although a low chance it could potentially stop the heart beat")
+    print("Incorrect, your current score is", score)
 
-print("Joji is a fairly known artist who used to be a Youtuber.")
-answerCinco = input("Name one of his Aliases when he was a Youtuber")
+print("Who is generally credited as the inventor of slap on electric bass?")
+answerCinco = input("Thumpin' and pluckin': ")
 
+if answerCinco.lower() == 'larry graham' or answerCinco.lower() == 'graham larry':
+    print("Correct!")
+    score = score+10
+    print("Your current score is", score)
+else:
+    print("I don't blame you but... The correct answer was Larry Graham")
+    print("Your current score is", score)
+
+print("Who is the Russian slap champion?")
+answerSeis = input("Be sure to correctly spell it: ")
+
+if answerSeis.lower() == 'vasiliy khamotiskiy':
+    print("You get extra points for this!")
+    score = score+20
+    print("Your current score is", score)
+else:
+    print("Better luck next time? The correct answer was Vasiliy Khamotiskiy")
+    print("Your current score is", score)
+
+print("Vasiliy Khamotiskiy was knocked out for the first time by who?")
+answerSeis = input("Be sure to correctly spell it: ")
+
+if answerSeis.lower() == 'vyacheslav zezulya':
+    print("Nicely done!")
+    score = score+20
+    print("Your current score is", score)
+else:
+    print("Incorrecto. The correct answer was Vasiliy Khamotiskiy")
+    print("Your current score is", score)
