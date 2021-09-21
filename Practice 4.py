c381a9754c888e881a8b4580709006f89559c2a5
toricden <toricden@urbandaleschools.com>
2021-09-21 11:05:03 -0500

    Save Seite

--- Practice 4.py
+++ Practice 4.py
@@ -1,11 +1,45 @@
-a = 5
-b = 5
-if a>b:
-    print("A is greater than B")
-if b>a:
-    print("B is greater than A")
-print ("Completed")
-if a>=b:
-    print("A is equal to or greater than B")
+#a = 4
+#b = 5
+#if a>b:
+    #print("A is greater than B")
+#if b>a:
+    #print("B is greater than A")
+#print ("Completed")
+#if a>=b:
+    #print("A is equal to or greater than B")
+#if a!=b:
+    #print ("A doesn't equal B")
 #double equals for if statementnts in comparison
 #single equals for assignment operator
+
+# and if, greater to equal to less than
+#a=False
+#b=3
+#c=1
+
+#if a<b and a<c:
+    #print("a is smaller than b and c")
+
+#if a<b or a<c:
+    #print("A isn't the biggest number")
+#if a==True:
+    #print ("A is true")
+#if not a==True:
+    #print ("A is false")
+
+#else if
+#temp = float (input("Hey, how hot is it today?"))
+
+#if temp>=90:
+    #print ("Woah that's hot!")
+#elif temp >=50:
+    #print ("It's a nice day out!")
+#else:
+    #print ("Man it's freezing!")
+
+jedi = (input ("Who is the greatest Jedi?  "))
+
+if jedi.lower()=="yoda":
+    print ("That is correct!")
+else:
+    print ("Wrong Jedi that is!")
