fbb0a75a0e9c7a04b8d7e330da85cc8a2ad9aaef
Joseph Jarman <57240500+Jarmander333@users.noreply.github.com>
2021-09-21 07:59:10 -0500

    Update 4.1_Number_Analysis.py
    Program made.

--- 4.1_Number_Analysis.py
+++ 4.1_Number_Analysis.py
@@ -18,3 +18,23 @@ Out:  Test 1: Odd
       Test 2: Negative
       Test 3: Exclusive
 '''
+print("Integer Test!") # Prints the name of the program
+
+test_num = float(input("Please input a number: ")) # Asks the user for the number
+
+if (test_num % 2) == 0: # Checks if the number is even
+    print("Test 1: Even")
+else: # If it's not even, it's odd
+    print("Test 1: Odd")
+
+if test_num < 0: # Checks if the number is negative
+    print("Test 2: Negative")
+elif test_num == 0:
+    print("Test 2: Zero")
+else: # If it's not negative, it's positive
+    print("Test 2: Positive")
+
+if -100 > test_num > 100: # Checks if the number is within -100 to 100
+    print("Test 3: Exclusive")
+else: # If it's not, it's outside the range
+    print("Test 3: Inclusive")
