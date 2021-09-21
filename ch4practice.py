909682a8b0e7ef24d440c9aad67df6cbc73b98f8
bakerkai <bakerkai@urbandaleschools.com>
2021-09-21 11:03:23 -0500

    things

---
+++ ch4practice.py
@@ -0,0 +1,37 @@
+'''a = 5
+b = 5
+if a<=b:
+    print("A was less than B")
+# = assignment operator
+# == comparison operator
+if b>a:
+    print("B was greater than a")
+
+<
+>
+<=
+>=
+print("Done")
+# AND/OR
+
+a=1
+b=2
+c=3
+
+if a<b and a<c:
+    print("a is smallest number")
+# AND/OR
+
+
+a = "Jedi"
+if not a:
+    print("A is true")
+
+# es and else if (elif)
+temp = float(input("Hey, how hot is it?"))
+
+if temp >=90:
+    print("Wow it is hot")
+else:
+    print("Go back to Canada")
+'''
