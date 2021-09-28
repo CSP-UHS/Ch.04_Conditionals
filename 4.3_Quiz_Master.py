fc0a5a099cb825e80f583a1f5469c0b2ad8c00e0
bakerkai <bakerkai@urbandaleschools.com>
2021-09-28 11:03:02 -0500

    finished quiz master

--- 4.3_Quiz_Master.py
+++ 4.3_Quiz_Master.py
@@ -8,52 +8,84 @@ Things to add:
 - Total grade by somehow adding up the total points
 
 '''
-# quiz that tests people qbout quizzes
+
+# quiz that tests people about random things
 print("Hello! This is a quiz about quizzes in school!")
 score = int(0)
 studentname = input("What is your name?")
 print("Hello", studentname, "! get ready to take the quiz!")
 
-q1 = int(input("What is the passing grade of Urbandale High School? (Do NOT include % because that was too much work to code)"))
-if q1 == 80:
+q1 = input("What is the passing grade of Urbandale High School? (Do NOT include % because that was too much work to code)")
+if q1 == '80':
     print("Good job", studentname, "You got the question right!")
     score  += 1
 else:
-    print("Lmao", studentname, "You suck how did you even pass school?")
+    print("How did you not know that it was 80%?")
 
-print("Enough of that, now its time for True or False questions.")
+print("You did good on the first one... how here's some hard ones ")
 
-q2 = input("Are you able to use your phone to search for answers on google while taking a test?")
-if q2 == 'False' or 'false':
-    print("Good job,", studentname, "clearly you are a good person.")
+q2 = input("How many cores and threads do the IMACs have?(Bryce)")
+if q2 == '2 cores and 4 threads':
+    print("Good job, clearly did research")
     score += 1
 else:
-    print("Wow... currently reporting you to your teachers right now.")
+    print("Wow Bryce, I'd expect you to know that one. (2 cores and 4 threads) ")
 
 q3 = input("Should you study for a test?")
 if q3 == 'true' or 'True' or "yes" or 'Yes':
     print("Good job", studentname)
     score += 1
 else:
-    print("Makes sense, considering that you are failing this.")
+    print("It's Yes cmon man")
 
-q4 = int(input("What is the range for getting an A grade? (Don't include % sign")
-if q4 == 93-100 or 93 - 100 or 93- 100 or 93 -100:
-    print("Good job,", studentname)
+q4 = input("What is the range for getting an A grade? (Don't include % sign)")
+if q4 == '93-100':
+    print("Good job, satisfactory performance ")
     score += 1
 else:
-    print("Eh, that's alright. Tough question.")
+    print("93-100 is that beautiful A grade")
 
-q5 = input("How do you use the projector? do you")
 print("A: Slap it until it works")
 print("B: Plug the HDMI into the computer and put it on the right input")
 print("C: Scream at it")
 print("D: Put in a tech support ticket ")
+q5 = input("Using the above questions, how do you use a projector?")
 if q5 == 'B':
     print("Good Job! You did it! Now tell Mr. Hermon your wise words.")
-    score +=1
-else: print("Wow, did I give Mr. Hermon this test by accident?")
+    score += 1
+else:
+    print("Wow, did I give Mr. Hermon this test by accident? It's B")
 
-finalscore = score /5
+q6 = input("Can fire have a shadow?")
+if q6 == 'Yes' or 'yes':
+    print("I don't even know that answer either so good job")
+    score += 1
+else:
+    print("Yes... somehow ")
+
+print("Time for the hard one.")
+
+q7 = input("Is Mr. Hermons first name Mark or Marc? NO CHEATING!")
+if q7 == 'Marc':
+    score += 1
+    print("Good one.")
+else:
+    print("Marc... good try though")
+
+print("Thanks for taking this test!")
+
+finalscore = (score /7)*100
+if finalscore >=90:
+    print("Good job, you got an A (A- is basically a knock off A")
+elif finalscore >=80:
+    print("good job, you got a B")
+elif finalscore >=70:
+    print("Sadly, you got a C")
+elif finalscore >=60:
+    print("Can't even compete with that D grade ")
+else:
+    print("YOur a failure")
+
+
 print("Your final score is", finalscore)
 
