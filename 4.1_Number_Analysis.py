0003b58c8d1bd654d2e062d2b88284788895e2fe
huynhlin <huynhlin@urbandaleschools.com>
2022-09-21 09:40:59 -0500

    everything except for 4.3

--- 4.1_Number_Analysis.py
+++ 4.1_Number_Analysis.py
@@ -18,3 +18,22 @@ Out:  Test 1: Odd
       Test 2: Negative
       Test 3: Exclusive
 '''
+o = float(input("Give me a number and I will determine if it is odd or even."))
+if o % 2 == 0:
+    print("It is an even number.")
+else:
+    print("It is an odd number.")
+
+p = float(input("Give me a number and I will determine if it is positive, negative, or zero."))
+if p == 0:
+    print("It is zero.")
+elif p>0:
+    print("It is positive.")
+else:
+    print("It is negative.")
+
+h = float(input("Give me a number and I will determine whether it is between -100 and 100, or if it isn't."))
+if h > -100 and h < 100:
+    print("It is inclusive.")
+else:
+    print("It is exclusive.")
--- 4.2_Grading_2.0.py
+++ 4.2_Grading_2.0.py
@@ -4,3 +4,13 @@ GRADING 2.0
 Copy your Grading 1.0 program and modify it to also print out the letter grade depending on the numerical grade.
 If they fail, tell them to "Transfer to Johnston!"
 '''
+g = float(input("What is your grade?"))
+
+if g > 90:
+    print("You got an A.")
+elif g > 80:
+    print("You got a B.")
+elif g > 70:
+    print("You got a C.")
+else:
+    print("Transfer to Johnston!")
