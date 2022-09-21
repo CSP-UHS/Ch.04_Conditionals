0354f918e68019eacceb192df7097b8619d5405a
sandoval-ordagicben <sandoval-ordagicben@urbandaleschools.com>
2022-09-21 09:20:01 -0500

    finsihed all

---
+++ ch4 practice.py
@@ -0,0 +1,27 @@
+# a=4
+# b=4
+# c=20
+# if a>b and b<c:
+#     print("a is equal to b")
+# print("done")
+# >=, <= greater than or equal to
+# == equal to
+# ! not equal
+# type out and
+# type or
+# "or" 1 has to be true, "and" both have to be true
+# if not - asking if false
+# if true print this. else code is if false then this.elif
+#
+
+a="yoda"
+b=5
+c=20
+d=3
+
+jedi= input("Who is the greatest jedi master of all time?")
+if jedi.lower()==a:
+    print("You are correct")
+else:
+    print("You are wrong!")
+print("The answer was",a,)
