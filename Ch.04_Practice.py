c46f9de04833cd6935c600b931ef48cb2a38893e
pattisonsam <pattisonsam@urbandaleschools.com>
2022-09-21 10:05:05 -0500

    4.0 done

---
+++ Ch.04_Practice.py
@@ -0,0 +1,27 @@
+a=4
+b=5
+c=20
+d=30
+if (a>b or a<c) and a<d: #third has to be true and then the or
+    print("LETS ME OUT")
+    print("Howdy")
+print("AAAAAaaaaaaaaaa!!!!!")
+#Needs two equal signs to test if something equals something else
+#!= means not equal to
+
+e=True
+if not e:
+    print("This was false")
+#to print something that is true just say if e, if not e, e is false
+
+if d==5:
+    print("Yay")
+else:
+    print("awww")
+
+if d<=a:
+    print("Hello")
+elif d>=a:
+    print("World")
+
+#if jedi.lower() make variable lower it makes any lower or upper correct
