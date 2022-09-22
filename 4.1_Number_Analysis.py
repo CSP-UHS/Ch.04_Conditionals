ad722965423662ec460b1c7f384bdd48ad1263d6
pattisonsam <pattisonsam@urbandaleschools.com>
2022-09-22 08:18:41 -0500

    4.1 done

--- 4.1_Number_Analysis.py
+++ 4.1_Number_Analysis.py
@@ -18,3 +18,27 @@ Out:  Test 1: Odd
       Test 2: Negative
       Test 3: Exclusive
 '''
+
+num = int(input("Enter a whole number"))
+print("--------------------------------")
+#test 1 odd or even
+if num%2==0:
+    print(num, "is even!")
+else:
+    print(num, "is odd!")
+print("--------------------------------")
+#test 2 positive or negative
+if num>0:
+    print(num, "is a positive!")
+elif num<0:
+    print(num, "is a negative!")
+else:
+    print(num, "is 0. Now I don't know if 0 is positive or negative."
+               "\nSo I decided to just make an extra program just to be sure.")
+print("--------------------------------")
+#test 3 between -100 and 100
+if num<=100 and num>=-100:
+    print(num, "is <=100 and >=-100")
+else:
+    print(num, "is not <=100 and >=-100")
+print("--------------------------------")
