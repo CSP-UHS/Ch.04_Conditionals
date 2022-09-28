ab5ce924b7ad21f9d096f77f8e8a0178cc71f5fe
evittstan <evittstan@urbandaleschools.com>
2022-09-28 09:17:01 -0500

    Number Analysis

--- 4.1_Number_Analysis.py
+++ 4.1_Number_Analysis.py
@@ -18,3 +18,21 @@ Out:  Test 1: Odd
       Test 2: Negative
       Test 3: Exclusive
 '''
+
+number = float(input("What is your favorite number?"))
+test1 = number%2
+print("hmmm interesting this will help with our reasearch")
+if test1 == 0:
+    print("Test 1: Even")
+else:
+    print("Test 1: Odd")
+if number > 0:
+    print("Test 2: Positive")
+else:
+    print("Test 2: Negative")
+if 100 > number > -100:
+    print("Test 3: Inclusive")
+else:
+    print("Test 3: Exclusive")
+print("ha now I have the exact coordinates of your house I will now send a scary Jack in the box to your door")
+print("It will arrive in 3-5 business days be afraid :)")
