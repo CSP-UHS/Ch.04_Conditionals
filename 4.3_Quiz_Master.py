7a24d5bd9c9d5949fc7cbbeab427286b51d59e8b
avismat <avismat@urbandaleschools.com>
2022-09-28 08:31:55 -0500

    done

--- 4.3_Quiz_Master.py
+++ 4.3_Quiz_Master.py
@@ -6,6 +6,17 @@ two of your student colleagues before you run it by your instructor.
 '''
 import time
 
+letterGrades = \
+    {"A": 93, "A-": 90, "B+": 87, "B": 83, "B-": 80, "C+": 77, "C": 73, "C-": 70, "D+": 67, "D": 63, "D-": 60, "F": 0}
+
+def overallGrade(sem,final,worth) :
+    return sem*(1-worth/100) + final*(worth/100)
+
+def toLetterGrade(percent) :
+    for i in letterGrades :
+        if percent >= letterGrades.get(i) :
+            return i
+
 class question():
     def __init__(self, prompt, answers, correctAnswer):
         # correctAnswer is an index of the answers list, not a number
@@ -16,7 +27,7 @@ class question():
 
     def checkAnswer(self):
         answerString = self.answers[self.correctAnswer]
-        self.isCorrect = (self.userInput.lower() == answerString.lower()) or (self.userInput == str(self.correctAnswer+1))
+        self.isCorrect = (self.userInput.lower().strip() == answerString.lower()) or (self.userInput == str(self.correctAnswer+1))
 
         if self.isCorrect:
             print(('\033[92m' + "\nYou are correct! {} is the answer!" + '\033[0m').format(answerString))
@@ -63,11 +74,23 @@ def main():
         question("Q4: What is the capital of Canada?", ["Ottawa", "Toronto", "Montreal", "Vancouver"], 0),
         question("Q5: 89 - 27 + 5", ["67"], -1),
         question("Q6: 2x + 62 = 74\nSolve for x", ["6"], -1),
-        question("Q7: Who is the principal of UHS?", ["Tim Carver","Mr Carver","Mr. Carver"], -1)
+        question("Q7: Who is the principal of UHS?", ["Mr. Carver"], -1)
     ]
 
+    correctCount = 0
+    wrongCount = 0
+
     for q in allQuestions:
-        q.ask()
+
+        askedQuestion = q.ask()
+        if askedQuestion:
+            correctCount += 1
+        else :
+            wrongCount += 1
+
         time.sleep(1)
+
+    percentage = (correctCount/(correctCount + wrongCount)) * 100
+    print("Thank you for taking the quiz!\nYou scored a {}% ({})".format(str(percentage)[0:5], toLetterGrade(percentage)))
 
 main()
