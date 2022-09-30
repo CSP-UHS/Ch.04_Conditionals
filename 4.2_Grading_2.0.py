286dda8d638a86d3bf43033038c1ba1ec1f91d11
terronezkad <terronezkad@urbandaleschools.com>
2022-09-30 08:52:48 -0500

    updated

--- 4.2_Grading_2.0.py
+++ 4.2_Grading_2.0.py
@@ -15,33 +15,33 @@ print("your overall grade in the class is",overall,"%")
 if overall>=93.0:
     print("Your letter grade is an A")
     print("Amazing Job!")
-elif overall>=90.0 and overall<=92.9:
+elif overall>=90.0:
     print("Your letter grade is an A-")
     print("Great Job!")
-elif overall>=87.0 and overall<=89.9:
+elif overall>=87.0:
     print("Your letter grade is a B+")
     print("Good Job!")
-elif overall>=83.0 and overall<=86.9:
+elif overall>=83.0:
     print("Your letter grade is a B")
     print("Nice Job!")
-elif overall>=80.0 and overall<=82.9:
+elif overall>=80.0:
     print("Your letter grade is a B-")
     print("Pretty good.")
-elif overall>=77.0 and overall<=79.9:
+elif overall>=77.0:
     print("Your letter grade is a C+")
     print("You did ok.")
-elif overall>=73.0 and overall<=76.9:
+elif overall>=73.0:
     print("Your letter grade is a C")
     print("Average.")
-elif overall>=70.0 and overall<=72.9:
+elif overall>=70.0:
     print("Your letter grade is a C-")
     print("Just enough.")
-elif overall>=67.0 and overall<=69.9:
+elif overall>=67.0:
     print("Your letter grade is a D+")
     print("Barely enough.")
-elif overall>=65.0 and overall<=66.9:
+elif overall>=65.0:
     print("Your letter grade is a D")
     print("At least you passed.")
-elif overall<65.0:
+else:
     print("FAILED!!!!!! YOU STINK")
     print("TRANSFER TO JOHNSTON!!!")
