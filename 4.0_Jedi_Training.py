4c384913582ffdf8b5a6667ae41384f1d5ee18ad
pattisonsam <pattisonsam@urbandaleschools.com>
2022-09-30 08:43:54 -0500

    This is stupid

--- 4.0_Jedi_Training.py
+++ 4.0_Jedi_Training.py
@@ -2,34 +2,34 @@
 
   #1. Make the following program work. (3 mistakes)
 
-midi = float(input("Enter midichlorian count: "))
-if midi > 10000:
-    print("You have serious Jedi potential")
-else:
-    print("Jedi, you will never be.")
+# midi = float(input("Enter midichlorian count: "))
+# if midi > 10000:
+#     print("You have serious Jedi potential")
+# else:
+#     print("Jedi, you will never be.")
 
 
  # 2. Make the following program work. (3 mistakes)
      
-x = float(input("Enter a number: "))
-if x==3:
-    print("You entered 3")
+# x = int(input("Enter a number: "))
+# if x==3:
+#     print("You entered 3")
 
 
   # 3. Make the following program work. (4 mistakes)
      
-answer = input("What is the name of Poe Dameron's Droid? ")
-if answer.upper()=="BB8":
-    print("Correct!")
-else:
-    print("Incorrect! It is BB8.")
+# answer = input("What is the name of Poe Dameron's Droid? ")
+# if answer.upper()=="BB8":
+#     print("Correct!")
+# else:
+#     print("Incorrect! It is BB8.")
 
 
   # 4. Make the following program work. (4 mistakes)
      
-jedi = input("Name one of the top 3 greatest Jedi?")
-if jedi.upper()=="YODA" or jedi.upper()=="LUKE_SKYWALKER" or jedi.upper()=="OBI-WAN_KENOBI":
-    print("That is correct!")
+# jedi = input("Name one of the top 3 greatest Jedi?")
+# if jedi.upper()=="YODA" or jedi.upper()=="LUKE SKYWALKER" or jedi.upper()=="OBI-WAN KENOBI":
+#     print("That is correct!")
 
 
 
@@ -47,6 +47,8 @@ if user_input.upper() == "A":
     sens = 1000
 elif user_input.upper() == "B":
     sens == 900
-else:
+elif user_input.upper() == "C":
     sens = 0
+else:
+    sens = "Not a choice"
 print("Sensitivity: ",sens)
