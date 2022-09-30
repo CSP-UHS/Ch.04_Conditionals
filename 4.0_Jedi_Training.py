d807e4b9bd80310a4cec889af5c5ad62dac09095
hensleytho <hensleytho@urbandaleschools.com>
2022-09-30 08:44:41 -0500

    4.0 jedi training fixed

--- 4.0_Jedi_Training.py
+++ 4.0_Jedi_Training.py
@@ -1,7 +1,7 @@
-# Sign your name:_Tommy Hensley_
+# # Sign your name:_Tommy Hensley_
+#
+#   #1. Make the following program work. (3 mistakes)
 
-  #1. Make the following program work. (3 mistakes)
-     
 midichlorians = float(input("Enter midichlorian count: "))
 if midichlorians > 10000:
     print("You have serious Jedi potential")
@@ -10,14 +10,14 @@ else:
 
 
  # 2. Make the following program work. (3 mistakes)
-     
+
 x = input("Enter a number: ")
 if x == 3:
     print("You entered 3")
 
 
   # 3. Make the following program work. (4 mistakes)
-     
+
 answer = input("What is the name of Poe Dameron's Droid? ")
 if answer == "BB8":
     print("Correct!")
@@ -26,9 +26,9 @@ else:
 
 
   # 4. Make the following program work. (4 mistakes)
-     
+
 jedi = input("Name one of the top 3 greatest Jedi.")
-if jedi == "Yoda" or jedi == "Luke Skywalker" or jedi =="Obi-Wan Kenobi" or jedi == "Odedi":
+if jedi.lower() == "yoda" or jedi.lower() == "luke skywalker" or jedi.lower() =="obi-wan kenobi":
     print("That is correct!")
 else:
     print("That is incorrect!")
@@ -36,7 +36,7 @@ else:
 
  # 5. Make the following program work whether they enter a, A, Jedi Master or jedi master
  #    Print "Not a choice!" if they don't choose any of the three and set sensitivity to blank text.
-     
+
 print("Welcome to the Jedi Academy!")
 
 print("A. Jedi Master")
@@ -46,12 +46,11 @@ print("C. Droid")
 user_input = input("Choose a character?")
 
 if user_input.lower() == "a" or user_input.lower() == "jedi master":
-         print("sensitivity = 1000")
+         sensitivity = 1000
 elif user_input.lower() == "b" or user_input.lower() == "sith lord":
-         print("sensitivity = 900")
+         sensitivity = 900
 elif user_input.lower() == "c" or user_input.lower() == "droid":
-         print("sensitivity = 0")
+         sensitivity = 0
 else:
-    print("Not a choice")
-
-print("Sensitivity:")
+    sensitivity = "Not a Choice"
+print("sensitivity:", sensitivity)
