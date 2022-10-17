b5775e71f3ad7897a8640e0dd5fc87ed14df7bc8
donohoand <donohoand@urbandaleschools.com>
2022-10-16 22:12:22 -0500

    ch.4_work

--- 4.1_Number_Analysis.py
+++ 4.1_Number_Analysis.py
@@ -18,3 +18,21 @@ Out:  Test 1: Odd
       Test 2: Negative
       Test 3: Exclusive
 '''
+num = float(input("what is the number?:"))
+
+if(num % 2) ==0:
+    print("{0} is Even".format(num))
+else:
+    print("{0} is Odd".format(num))
+
+if num > 0:
+    print("positive number")
+elif num == 0:
+    print("zero")
+else:
+    print("negative number")
+
+if num >= -100 and num <= 100:
+    print("Inclusive")
+else:
+    print("Exclusive")
--- 4.2_Grading_2.0.py
+++ 4.2_Grading_2.0.py
@@ -4,3 +4,23 @@ GRADING 2.0
 Copy your Grading 1.0 program and modify it to also print out the letter grade depending on the numerical grade.
 If they fail, tell them to "Transfer to Johnston!"
 '''
+Sem_Grade = float(input("sem grade:"))
+Final_Exam = float(input("Final Exam"))
+Exam_worth = float(input("Exam_Worth"))
+Answer = ((Sem_Grade*(100-Exam_worth)+(Final_Exam*Exam_worth))/100)
+Grade_Percent = Answer
+
+if Grade_Percent >= 100:
+    Msg = "Awesome Job you got all questions correct for an A+"
+elif  Grade_Percent > 90:
+    Msg = "Great Job you Got an A"
+elif  Grade_Percent >= 80:
+    Msg = "Great Job you Got an B"
+elif  Grade_Percent >= 70:
+    Msg = "Great Job you Got an C"
+elif  Grade_Percent >= 60:
+    Msg = "You barely passed with an D"
+else:
+    Msg = "You failed you loser!! don't get a F again loser"
+
+print(Msg)
