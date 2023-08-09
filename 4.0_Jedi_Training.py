1d67c9e125c338e9f67ffee03736f7dd42f70693
Marc Hermon <hermonm@urbandaleschools.com>
2023-08-09 15:56:09 -0500

    Update 4.0_Jedi_Training.py

--- 4.0_Jedi_Training.py
+++ 4.0_Jedi_Training.py
@@ -1,6 +1,6 @@
-# Sign your name:________________
+# 4.0 Jedi Training (pts)  Name:________________
 
-  #1. Make the following program work. (3 mistakes)
+  #1. Make the following program work. (3 mistakes)  (3pts)
      
      midichlorians = float(input("Enter midichlorian count: ")
      if midichlorians > 10000
@@ -9,14 +9,14 @@
          print("Jedi, you will never be.")
 
 
- # 2. Make the following program work. (3 mistakes)
+ # 2. Make the following program work. (3 mistakes)  (3pts)
      
      x = input("Enter a number: ")
      if x = 3
          print("You entered 3")
 
 
-  # 3. Make the following program work. (4 mistakes)
+  # 3. Make the following program work. (4 mistakes)  (4pts)
      
      answer = input("What is the name of Poe Dameron's Droid? ")
      if a = "BB8":
@@ -25,7 +25,7 @@
          print("Incorrect! It is BB8.")
 
 
-  # 4. Make the following program work. (4 mistakes)
+  # 4. Make the following program work. (4 mistakes) (4pts)
      
      x = input("Name one of the top 3 greatest Jedi.")
      if jedi == Yoda or Luke Skywalker or Obi-Wan Kenobi:
@@ -34,7 +34,7 @@
 
 
  # 5. Make the following program work whether they enter a, A, Jedi Master or jedi master
- #    Print "Not a choice!" if they don't choose any of the three and set sensitivity to blank text.
+ #    Print "Not a choice!" if they don't choose any of the three and set sensitivity to blank text.  (5pts)
      
      print("Welcome to the Jedi Academy!")
 
