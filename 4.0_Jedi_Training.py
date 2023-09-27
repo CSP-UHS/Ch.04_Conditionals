f0885c6080008d13cf2b0d2d68f6595059b1d283
Marc Hermon <hermonm@urbandaleschools.com>
2023-09-27 12:01:17 -0500

    Update 4.0_Jedi_Training.py

--- 4.0_Jedi_Training.py
+++ 4.0_Jedi_Training.py
@@ -1,58 +1,72 @@
 # 4.0 Jedi Training (40pts)  Name:________________
-#Below each program list the mistakes found in comments.
+#BELOW EACH PROGRAM LIST THE MISTAKES FOUND
 
-  #1. Make the following program work. (3 mistakes)  (3pts)
-     
-     midichlorians = float(input("Enter midichlorian count: ")
-     if midichlorians > 10000
-         print("You have serious Jedi potential")
-     elif:
-         print("Jedi, you will never be.")
+#1. Make the following program work. (3 mistakes)  (3pts)
+   
+   midichlorians = float(input("Enter midichlorian count: ")
+   if midichlorians > 10000
+       print("You have serious Jedi potential")
+   elif:
+       print("Jedi, you will never be.")
+#1
+#2
+#3                         
+
+# 2. Make the following program work. (3 mistakes)  (3pts)
+   
+   x = input("Enter a number: ")
+   if x = 3
+       print("You entered 3")
+
+#1
+#2
+#3
+
+# 3. Make the following program work. (4 mistakes)  (4pts)
+   
+   answer = input("What is the name of Poe Dameron's Droid? ")
+   if a = "BB8":
+       print("Correct!")
+       else
+       print("Incorrect! It is BB8.")
+
+#1
+#2
+#3
+#4
+
+# 4. Make the following program work. (4 mistakes) (4pts)
+   
+   x = input("Name one of the top 3 greatest Jedi.")
+   if jedi == Yoda or Luke Skywalker or Obi-Wan Kenobi:
+       print "That is correct!"
 
 
- # 2. Make the following program work. (3 mistakes)  (3pts)
-     
-     x = input("Enter a number: ")
-     if x = 3
-         print("You entered 3")
+#1
+#2
+#3
+#4
 
 
-  # 3. Make the following program work. (4 mistakes)  (4pts)
-     
-     answer = input("What is the name of Poe Dameron's Droid? ")
-     if a = "BB8":
-         print("Correct!")
-         else
-         print("Incorrect! It is BB8.")
+# 5. Make the following program work whether they enter a, A, Jedi Master or jedi master
+#    Print "Not a choice!" if they don't choose any of the three and set sensitivity to blank text.  (6pts)
+   
+   print("Welcome to the Jedi Academy!")
 
+   print("A. Jedi Master")
+   print("B. Sith Lord")
+   print("C. Droid")
 
-  # 4. Make the following program work. (4 mistakes) (4pts)
-     
-     x = input("Name one of the top 3 greatest Jedi.")
-     if jedi == Yoda or Luke Skywalker or Obi-Wan Kenobi:
-         print "That is correct!"
+   user_input = input("Choose a character?")
 
+   if user_input = A:
+       sensitivity = 1000
+   else if user_input = B:
+       sensitivity = 900
+   else if user_input = C:
+       sensitivity = 0
 
-
- # 5. Make the following program work whether they enter a, A, Jedi Master or jedi master
- #    Print "Not a choice!" if they don't choose any of the three and set sensitivity to blank text.  (6pts)
-     
-     print("Welcome to the Jedi Academy!")
-
-     print("A. Jedi Master")
-     print("B. Sith Lord")
-     print("C. Droid")
-
-     user_input = input("Choose a character?")
-
-     if user_input = A:
-         sensitivity = 1000
-     else if user_input = B:
-         sensitivity = 900
-     else if user_input = C:
-         sensitivity = 0
-
-     print("Sensitivity: ",Sensitivity)
+   print("Sensitivity: ",Sensitivity)
 
 
 
@@ -69,13 +83,13 @@ A small report will then be printed. Use the following to test your program:
 
 In: 32  
 Out:  Test 1: Even
-      Test 2: Positive
-      Test 3: Inclusive
+    Test 2: Positive
+    Test 3: Inclusive
 
 In: -123  
 Out:  Test 1: Odd
-      Test 2: Negative
-      Test 3: Exclusive
+    Test 2: Negative
+    Test 3: Exclusive
 '''
 
 
